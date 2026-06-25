29d7e2177277bea192315153171e84890db90ebf
Guillaume <gthvn1@gmail.com>
2026-06-25 09:11:36 +0200

    reorder .note

--- xen-pv64/src/boot.s
+++ xen-pv64/src/boot.s
@@ -10,6 +10,22 @@
  */
 .section .note.Xen, "a", @note
 
+/* XEN_ELFNOTE_HYPERCALL_PAGE = 2: address Xen will fill with stubs */
+.align 4
+.long 4                     /* namesz */
+.long 8                     /* descsz: 64-bit address */
+.long 2
+.asciz "Xen"
+.quad hypercall_page
+
+/* XEN_ELFNOTE_XEN_VERSION = 5: must be exactly "xen-3.0" */
+.align 4
+.long 4                     /* namesz */
+.long 8                     /* descsz: "xen-3.0\0" = 8 */
+.long 5
+.asciz "Xen"
+.asciz "xen-3.0"
+
 /* XEN_ELFNOTE_GUEST_OS = 6: identify the OS, any string is fine */
 .align 4
 .long 4                     /* namesz: "Xen\0" = 4 */
@@ -35,21 +51,13 @@
 .asciz "Xen"
 .asciz "generic"
 
-/* XEN_ELFNOTE_HYPERCALL_PAGE = 2: address Xen will fill with stubs */
+/* XEN_ELFNOTE_PAE_MODE = 9 */
 .align 4
 .long 4                     /* namesz */
-.long 8                     /* descsz: 64-bit address */
-.long 2
+.long 4                     /* descsz: "yes\0" = 4 */
+.long 9
 .asciz "Xen"
-.quad hypercall_page
-
-/* XEN_ELFNOTE_XEN_VERSION = 5: must be exactly "xen-3.0" */
-.align 4
-.long 4                     /* namesz */
-.long 8                     /* descsz: "xen-3.0\0" = 8 */
-.long 5
-.asciz "Xen"
-.asciz "xen-3.0"
+.asciz "yes"
 
 /* XEN_ELFNOTE_FEATURES = 10: require non-writable page tables (PV safety) */
 .align 4
@@ -59,14 +67,6 @@
 .asciz "Xen"
 .asciz "!writable_page_tables|pae_pgdir_above_4gb"
 .align 4                    /* pad descsz 42 -> 44 */
-
-/* XEN_ELFNOTE_PAE_MODE = 9 */
-.align 4
-.long 4                     /* namesz */
-.long 4                     /* descsz: "yes\0" = 4 */
-.long 9
-.asciz "Xen"
-.asciz "yes"
 
 
 /*
